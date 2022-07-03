d5e81ed44c3b9a0982d678c434875e6552905d29
Arnel Sanchez Rodríguez <arnelsanchezrodriguez@gmail.com>
2022-07-03 00:50:07 -0400

    Update text

--- Proyecto/admin.py
+++ Proyecto/admin.py
@@ -20,28 +20,25 @@ class Admin():
                 self.username = ""
         elif list[0] == "remove_user":
             self.username = list[1]
-        elif list[0] == "remove_pass":
-            self.password = list[1]
-            if self.username != "":
-                users = ""
-                with open(self.path, 'rb') as f:
-                    while True:
-                        bytes_read = f.read()
-                        if bytes_read == b'':
-                            break
-                        else:
-                            users+=str(bytes_read)
-                users = str(users).replace("b'", '')
-                users = str(users).replace("'", '')
-                users = users.split("\\r\\n****\\r\\n")
-                write = ""
-                for i in range(1,len(users)-1,3):
-                    if str(users[i])!=str(self.username):
-                        write += f"\r\n****\r\n{users[i]}\r\n****\r\n{users[i+1]}\r\n****\r\n"
-                with open(self.path, 'w') as f:
-                    f.write(write)
-                self.password = ""
-                self.username = ""
+            users = ""
+            with open(self.path, 'rb') as f:
+                while True:
+                    bytes_read = f.read()
+                    if bytes_read == b'':
+                        break
+                    else:
+                        users+=str(bytes_read)
+            users = str(users).replace("b'", '')
+            users = str(users).replace("'", '')
+            users = users.split("\\r\\n****\\r\\n")
+            write = ""
+            for i in range(1,len(users)-1,3):
+                if str(users[i])!=str(self.username):
+                    write += f"\r\n****\r\n{users[i]}\r\n****\r\n{users[i+1]}\r\n****\r\n"
+            with open(self.path, 'w') as f:
+                f.write(write)
+            self.password = ""
+            self.username = ""
                     
                     
 if __name__ == "__main__":
